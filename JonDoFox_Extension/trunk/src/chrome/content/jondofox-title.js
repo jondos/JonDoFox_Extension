ecf6b63eb55c865251ff8f6fa2df3c9648f5e2a8
renner <renner@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2008-09-26 13:30:16 +0000

    Pre-commit for being able to rename another file to jondofox-overlay.js

---
+++ JonDoFox_Extension/trunk/src/chrome/content/jondofox-title.js
@@ -0,0 +1,81 @@
+/******************************************************************************
+ * Copyright (C) 2008, JonDos GmbH
+ * Author: Johannes Renner
+ *
+ *****************************************************************************/
+
+///////////////////////////////////////////////////////////////////////////////
+// Debug stuff
+///////////////////////////////////////////////////////////////////////////////
+
+// Dump information to the console?
+var mDebug = true;
+
+// Send data to the console if we're in debug mode
+// Don't forget to create 'browser.dom.window.dump.enabled' first!
+function log(msg) {
+  if (mDebug) dump("JDFTitle :: " + msg + "\n");
+}
+
+log("TEST");
+
+///////////////////////////////////////////////////////////////////////////////
+// Append a JonDoFox customized string to the window's title
+///////////////////////////////////////////////////////////////////////////////
+
+// Get the preferences handler
+var prefsHandler = Components.classes['@jondos.de/preferences-handler;1'].
+                                 getService().wrappedJSObject;
+
+// Get the appInfo service
+var appInfo = Components.classes['@mozilla.org/xre/app-info;1'].
+                            getService(Components.interfaces.nsIXULAppInfo);
+
+// Get both of the version strings and the application's name
+var versionPref = "extensions.jondofox.profile_version";
+var profileVersion = prefsHandler.getStringPref(versionPref);
+var appVersion = appInfo.version;
+var appName = appInfo.name;
+
+// Create an appendix for the title string
+const titleString = "JonDoFox "+profileVersion+" ("+appName+" "+appVersion+")";
+
+// FIXME: This does not work on Macs
+// Set the title modifier
+function setTitleModifier() {
+  log("Setting title modifier");
+  try {
+    // Set the 'titlemodifier' attribute in the current document
+    var modAtt = document.documentElement.getAttribute("titlemodifier");
+    if (modAtt) {
+      document.documentElement.setAttribute("titlemodifier", titleString);
+    }
+    // This throws an exception if this.docShell is not set
+    if (this.docShell) {
+      //log("'this.docShell' is not null");
+      document.getElementById("content").updateTitlebar();
+    }
+  } catch (e) {
+    log("setTitleModifier(): " + e);
+  }
+}
+
+function initTitleListener() {
+  try {
+    // Add event listener to the content element
+    document.getElementById("content").addEventListener("DOMTitleChanged", 
+                                          setTitleModifier, false);
+  
+    // XXX: Not needed? .. mTabContainer ..
+    //gBrowser.addEventListener("DOMNodeInserted", setTitleModifier, true);
+    //gBrowser.addEventListener("DOMNodeRemoved", setTitleModifier, true);
+ 
+  } catch (e) {
+    log("initTitleListener(): " + e);
+  }
+}
+
+// Log something initially
+log("New window, this is " + titleString);
+setTitleModifier();
+window.addEventListener("load", initTitleListener, false);
