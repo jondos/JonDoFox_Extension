b58fe5566f9ac81dd3ebbfc00e64b660f98c38f3
renner <renner@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2009-03-15 21:42:15 +0000

    Added a working checkbox to switch the set_referrer preference

--- src/chrome/content/dialogs/prefs-dialog.js
+++ src/chrome/content/dialogs/prefs-dialog.js
@@ -1,10 +1,20 @@
+/******************************************************************************
+ * Copyright (c) 2009, Johannes Renner
+ *
+ * Preferences dialog javascript code
+ *****************************************************************************/
+
+// Indices of the single tabs
+const TABINDEX_GENERAL = 0;
+const TABINDEX_CUSTOMPROXY = 1;
+
 // Set the prefs handler, it will be needed
 var prefsHandler = Components.classes['@jondos.de/preferences-handler;1'].
-                                 getService().wrappedJSObject;
+    getService().wrappedJSObject;
 
 // JonDoFox-Manager is used for setting the proxy to custom
 var jdfManager = Components.classes['@jondos.de/jondofox-manager;1'].
-                                 getService().wrappedJSObject;
+    getService().wrappedJSObject;
 
 // Prefix for custom proxy settings
 var prefix = "extensions.jondofox.custom.";
@@ -13,132 +23,175 @@ var prefix = "extensions.jondofox.custom.";
 function log(msg) {
   dump("Dialog :: " + msg + "\n");
 }
-    
-// Load values into the dialog's elements
+
+// Separately load the prefs into both tabs
 function onLoad() {
   log("Init dialog, loading values ..");
-  try {       
-    // Get the label of the custom proxy
+  try {
+    loadPrefsGeneral();
+    loadPrefsCustomProxy();
+  } catch (e) {
+    log("onLoad(): " + e);    
+  }
+}
+
+// Load general preferences into the dialog
+function loadPrefsGeneral() {
+  log("Loading general preferences");
+  try {
+    // 'set-referrer' is a checkbox
+    document.getElementById('set-referrer').checked = 
+        prefsHandler.getBoolPref('extensions.jondofox.set_referrer');
+  } catch (e) {
+    log("loadPrefsGeneral(): " + e);    
+  }
+}
+
+// Write the general preferences
+function writePrefsGeneral() {
+  log("Write prefs general");
+  try {
+    prefsHandler.setBoolPref('extensions.jondofox.set_referrer',
+        document.getElementById('set-referrer').checked);
+  } catch (e) {
+    log("writePrefsGeneral(): " + e);
+  }
+}
+ 
+// Load custom proxy preferences into the dialog
+function loadPrefsCustomProxy() {
+  log("Loading custom proxy preferences");
+  try {
+    // Get the custom proxy label
     var label = prefsHandler.getStringPref(prefix + 'label');    
     if (label == "") {
-      // If label is empty, get the default label ..
+      // If label is empty, get the default label
       label = jdfManager.getString('jondofox.statusbar.label.custom');
     }
     // Set the label to the textfield
     document.getElementById('custom-label').value = label;
     // Get host and port settings for different protocols
     document.getElementById('http_host').value = 
-                prefsHandler.getStringPref(prefix + 'http_host');
+        prefsHandler.getStringPref(prefix + 'http_host');
     document.getElementById('http_port').value = 
-                prefsHandler.getIntPref(prefix + 'http_port');
+        prefsHandler.getIntPref(prefix + 'http_port');
     document.getElementById('ssl_host').value = 
-                prefsHandler.getStringPref(prefix + 'ssl_host');
+        prefsHandler.getStringPref(prefix + 'ssl_host');
     document.getElementById('ssl_port').value = 
-                prefsHandler.getIntPref(prefix + 'ssl_port'); 
+        prefsHandler.getIntPref(prefix + 'ssl_port'); 
     document.getElementById('ftp_host').value = 
-                prefsHandler.getStringPref(prefix + 'ftp_host');
+        prefsHandler.getStringPref(prefix + 'ftp_host');
     document.getElementById('ftp_port').value = 
-                prefsHandler.getIntPref(prefix + 'ftp_port');
+        prefsHandler.getIntPref(prefix + 'ftp_port');
     document.getElementById('gopher_host').value = 
-                prefsHandler.getStringPref(prefix + 'gopher_host');
+        prefsHandler.getStringPref(prefix + 'gopher_host');
     document.getElementById('gopher_port').value = 
-                prefsHandler.getIntPref(prefix + 'gopher_port'); 
+        prefsHandler.getIntPref(prefix + 'gopher_port'); 
     document.getElementById('socks_host').value = 
-                prefsHandler.getStringPref(prefix + 'socks_host');
+        prefsHandler.getStringPref(prefix + 'socks_host');
     document.getElementById('socks_port').value = 
-                prefsHandler.getIntPref(prefix + 'socks_port');        
+        prefsHandler.getIntPref(prefix + 'socks_port');        
     // Get socks version
     var version = prefsHandler.getIntPref(prefix + 'socks_version');
     if (version == 4) {
       document.getElementById('socks_version').selectedItem = 
-                  document.getElementById('version4');
+          document.getElementById('version4');
     } else {
       document.getElementById('socks_version').selectedItem = 
-                  document.getElementById('version5'); 
+          document.getElementById('version5'); 
     }
     // Get 'no_proxies_on'
     document.getElementById('no_proxies_on').value = 
-                prefsHandler.getStringPref(prefix + 'no_proxies_on');
+        prefsHandler.getStringPref(prefix + 'no_proxies_on');
   } catch (e) {
-    log("initDialog(): " + e);
-  }     
+    log("loadPrefsCustomProxy(): " + e);
+  }
 }
-   
+
 // Store the values
-function storeValues() {
-  log("Store values"); 
+function writePrefsCustomProxy() {
+  log("Write prefs custom proxy"); 
   try {
     // Set the label
     prefsHandler.setStringPref(prefix + 'label',
-                    document.getElementById('custom-label').value);
+        document.getElementById('custom-label').value);
     // Set single proxies
     prefsHandler.setStringPref(prefix + 'http_host', 
-                    document.getElementById('http_host').value);
+        document.getElementById('http_host').value);
     prefsHandler.setIntPref(prefix + 'http_port', 
-                    document.getElementById('http_port').value);
+        document.getElementById('http_port').value);
     prefsHandler.setStringPref(prefix + 'ssl_host', 
-                    document.getElementById('ssl_host').value);
+        document.getElementById('ssl_host').value);
     prefsHandler.setIntPref(prefix + 'ssl_port', 
-                    document.getElementById('ssl_port').value);
+        document.getElementById('ssl_port').value);
     prefsHandler.setStringPref(prefix + 'ftp_host', 
-                    document.getElementById('ftp_host').value);
+        document.getElementById('ftp_host').value);
     prefsHandler.setIntPref(prefix + 'ftp_port', 
-                    document.getElementById('ftp_port').value);
+        document.getElementById('ftp_port').value);
     prefsHandler.setStringPref(prefix + 'gopher_host', 
-                    document.getElementById('gopher_host').value);
+        document.getElementById('gopher_host').value);
     prefsHandler.setIntPref(prefix + 'gopher_port', 
-                    document.getElementById('gopher_port').value);
+        document.getElementById('gopher_port').value);
     prefsHandler.setStringPref(prefix + 'socks_host', 
-                    document.getElementById('socks_host').value);
+        document.getElementById('socks_host').value);
     prefsHandler.setIntPref(prefix + 'socks_port', 
-                    document.getElementById('socks_port').value);        
+        document.getElementById('socks_port').value);        
     // Set socks version
     prefsHandler.setIntPref(prefix + 'socks_version', 
-                    document.getElementById('socks_version').
-                       selectedItem.value);
+        document.getElementById('socks_version').selectedItem.value);
     // Set exceptions
     prefsHandler.setStringPref(prefix + 'no_proxies_on', 
-                    document.getElementById('no_proxies_on').value);
+        document.getElementById('no_proxies_on').value);
   } catch (e) {
-    log("storeValues(): " + e);
+    log("writePrefsCustomProxy(): " + e);
   }
 }
 
 // This is called on clicking the 'accept'-button
 function onAccept() {
   try {
-    // Store values
-    storeValues();
-    // If the current state is 'custom', reset the state
+    // Store all preferences
+    writePrefsGeneral();
+    writePrefsCustomProxy();
+    // If the current state is 'custom': reset it
     if (prefsHandler.getStringPref('extensions.jondofox.proxy.state') == 
-           'custom') {
-      enable();
+        'custom') {
+      setProxyCustom();
     }
   } catch (e) {
     log("onAccept(): " + e);
   }
-  // Return true in any case to close the window
+  // Return true in any case for closing the window
   return true;
 }
 
-// Apply configuration
+// Apply preferences from the current tab
 function onApply() {
   try {
-    // Store the configuration
-    storeValues();
-    enable();
+    // Get the index of the currently selected tab
+    var index = document.getElementById('tab-box').selectedIndex;
+    // Call the respective write-method
+    if (index == TABINDEX_GENERAL) {
+      writePrefsGeneral();
+    } else if (index == TABINDEX_CUSTOMPROXY) {
+      writePrefsCustomProxy();
+      setProxyCustom();
+    } else {
+      // Should not happen
+      log("Crazy index: " + index);
+    }
   } catch (e) {
     log("onApply(): " + e);
   }
 }
 
 // Set the proxy state to 'custom'
-function enable() {
+function setProxyCustom() {
   try {
-    // Enable the custom proxy from jdfManager
+    // Enable the custom proxy using jdfManager
     jdfManager.setProxy('custom');
   } catch (e) {
-    log("enable(): " + e);
+    log("setProxyCustom(): " + e);
   }
 }
 
@@ -158,3 +211,4 @@ function enableOptions() {
   }
   // TODO: Disable textfields
 }
+
