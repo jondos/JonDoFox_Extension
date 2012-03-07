29f40fe844be3a4ddfd194f1d84e2168bd7166a4
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-03-07 14:23:17 +0000

    updated xpi to 0.2.19

--- JonDoFox_Extension/trunk/src/components/jondofox-manager.js
+++ JonDoFox_Extension/trunk/src/components/jondofox-manager.js
@@ -1135,6 +1135,8 @@ JDFManager.prototype = {
             'extensions.jondofox.profile_version') !== "2.6.2" &&
           this.prefsHandler.getStringPref(
             'extensions.jondofox.profile_version') !== "2.6.3" && 
+          this.prefsHandler.getStringPref(
+            'extensions.jondofox.profile_version') !== "2.6.4" &&
           this.prefsHandler.getBoolPref('extensions.jondofox.update_warning')) {
           this.jdfUtils.showAlertCheck(this.jdfUtils.
             getString('jondofox.dialog.attention'), this.jdfUtils.
