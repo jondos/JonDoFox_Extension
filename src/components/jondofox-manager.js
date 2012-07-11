63d62a14e52cc5a21d7aa69511f989dede396a93
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-07-11 09:57:42 +0000

    removed pdf plugin warning as we disable all plugins (but flash) in JonDo mode

--- src/components/jondofox-manager.js
+++ src/components/jondofox-manager.js
@@ -1502,17 +1502,6 @@ JDFManager.prototype = {
           checkboxNews.addEventListener("click", function() {JDFManager.
 		    checkboxNewsChecked(checkboxNews, type);}, false);
         }
-      } else if (this.arguments && this.arguments[0]) {
-        log("We got probably a commonDialog...");
-        dialogParam = this.arguments[0].QueryInterface(CI.nsIDialogParamBlock);
-        log("Let's check whether we've got a NoScript pdf-dialog...");
-        dialogMessage = dialogParam.GetString(0).substr(0,10000).trim().
-                        toLowerCase();
-        if (dialogMessage.indexOf(".pdf") !== -1) {
-          this.document.loadOverlay(
-               "chrome://jondofox/content/external-pdfPlugin.xul", null);
-          this.setTimeout(JDFManager.prototype.showWarning, 200, this, true, false);
-        }
       } else {
         log("Nothing found...");
       } 
