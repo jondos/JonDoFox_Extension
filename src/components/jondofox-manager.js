f6109a844b8ace47ebb8367d580192f6d089ebbe
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-07-17 07:21:03 +0000

    final 0.2.22 version

--- src/components/jondofox-manager.js
+++ src/components/jondofox-manager.js
@@ -1149,6 +1149,8 @@ JDFManager.prototype = {
             'extensions.jondofox.profile_version') !== "2.6.5" && 
           this.prefsHandler.getStringPref(
             'extensions.jondofox.profile_version') !== "2.6.6" &&
+          this.prefsHandler.getStringPref(
+            'extensions.jondofox.profile_version') !== "2.6.7" &&
           this.prefsHandler.getBoolPref('extensions.jondofox.update_warning')) {
           this.jdfUtils.showAlertCheck(this.jdfUtils.
             getString('jondofox.dialog.attention'), this.jdfUtils.
