b2f7830d882f8142f7bfaf308e8b4d019c9ce2e2
koppen <koppen@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2012-11-20 12:27:53 +0000

    updated CHANGELOG; updated preferences (UA + safebrowsing)

--- JonDoFox_Extension/trunk/src/components/jondofox-manager.js
+++ JonDoFox_Extension/trunk/src/components/jondofox-manager.js
@@ -1159,6 +1159,8 @@ JDFManager.prototype = {
             'extensions.jondofox.profile_version') !== "2.6.8" &&
           this.prefsHandler.getStringPref(
             'extensions.jondofox.profile_version') !== "2.6.9" &&
+          this.prefsHandler.getStringPref(
+            'extensions.jondofox.profile_version') !== "2.6.10" &&
           this.prefsHandler.getBoolPref('extensions.jondofox.update_warning')) {
           this.jdfUtils.showAlertCheck(this.jdfUtils.
             getString('jondofox.dialog.attention'), this.jdfUtils.
