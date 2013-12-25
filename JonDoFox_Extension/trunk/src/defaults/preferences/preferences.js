250e0850b387d3825af21579ee8e30ee5b56ea24
ness <ness@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2013-12-25 01:29:27 +0000

    Update to version 0.5.1

--- JonDoFox_Extension/trunk/src/defaults/preferences/preferences.js
+++ JonDoFox_Extension/trunk/src/defaults/preferences/preferences.js
@@ -86,17 +86,17 @@ pref("extensions.jondofox.tor.buildID_override","0");
 pref("extensions.jondofox.tor.oscpu_override", "Windows NT 6.1");
 pref("extensions.jondofox.tor.platform_override","Win32");
 pref("extensions.jondofox.tor.productsub_override","20100101");
-pref("extensions.jondofox.tor.useragent_override", "Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0");
+pref("extensions.jondofox.tor.useragent_override", "Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0");
 pref("extensions.jondofox.tor.useragent_vendor", "");
 pref("extensions.jondofox.tor.useragent_vendorSub","");
 
 // SafeBrowsing provider
 pref("extensions.jondofox.safebrowsing.provider.0.gethashURL",
-    "http://safebrowsing.clients.google.com/safebrowsing/gethash?client=navclient-auto-ffox&appver=17.0&pver=2.2");
+    "http://safebrowsing.clients.google.com/safebrowsing/gethash?client=navclient-auto-ffox&appver=24.0&pver=2.2");
 pref("extensions.jondofox.safebrowsing.provider.0.keyURL",
-    "https://sb-ssl.google.com/safebrowsing/newkey?client=navclient-auto-ffox&appver=17.0&pver=2.2");
+    "https://sb-ssl.google.com/safebrowsing/newkey?client=navclient-auto-ffox&appver=24.0&pver=2.2");
 pref("extensions.jondofox.safebrowsing.provider.0.lookupURL",
-    "http://safebrowsing.clients.google.com/safebrowsing/lookup?sourceid=firefox-antiphish&features=TrustRank&client=navclient-auto-ffox&appver=17.0&");
+    "http://safebrowsing.clients.google.com/safebrowsing/lookup?sourceid=firefox-antiphish&features=TrustRank&client=navclient-auto-ffox&appver=24.0&");
 pref("extensions.jondofox.safebrowsing.provider.0.reportErrorURL",
     "http://en-US.phish-error.mozilla.com/?hl=en-US");
 pref("extensions.jondofox.safebrowsing.provider.0.reportGenericURL",
@@ -110,7 +110,7 @@ pref("extensions.jondofox.safebrowsing.provider.0.reportPhishURL",
 pref("extensions.jondofox.safebrowsing.provider.0.reportURL",
     "http://safebrowsing.clients.google.com/safebrowsing/report?");
 pref("extensions.jondofox.safebrowsing.provider.0.updateURL",
-    "http://safebrowsing.clients.google.com/safebrowsing/downloads?client=navclient-auto-ffox&appver=17.0&pver=2.2");
+    "http://safebrowsing.clients.google.com/safebrowsing/downloads?client=navclient-auto-ffox&appver=24.0&pver=2.2");
 
 pref("extensions.jondofox.safebrowsing.warning.infoURL", "http://www.mozilla.com/en-US/firefox/phishing-protection/");
 
