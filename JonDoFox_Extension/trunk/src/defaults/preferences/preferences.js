b3b1485f38cf0b969b96690cf41781f54619228e
ness <ness@f4013887-1060-43d1-a937-9d2a4c6f27f8>
2014-07-22 19:45:34 +0000

    Release 0.6.6

--- JonDoFox_Extension/trunk/src/defaults/preferences/preferences.js
+++ JonDoFox_Extension/trunk/src/defaults/preferences/preferences.js
@@ -68,7 +68,7 @@ pref("extensions.jondofox.tor.socks_port", 9050);
 // Useragent settings JonDo
 pref("extensions.jondofox.jondo.appname_override", "Netscape");
 pref("extensions.jondofox.jondo.appversion_override", "5.0 (X11)");
-pref("extensions.jondofox.jondo.buildID_override", "20140609182057");
+pref("extensions.jondofox.jondo.buildID_override", "20140714155506");
 pref("extensions.jondofox.jondo.oscpu_override", "Linux i686");
 pref("extensions.jondofox.jondo.platform_override", "Linux i686");
 pref("extensions.jondofox.jondo.productsub_override", "20100101");
@@ -100,11 +100,11 @@ pref("extensions.jondofox.tor.default_charset", "ISO-8859-1");
 // Useragent settings FF Windows
 pref("extensions.jondofox.windows.appname_override", "Netscape");
 pref("extensions.jondofox.windows.appversion_override", "5.0 (Windows)");
-pref("extensions.jondofox.windows.buildID_override", "20140605174243");
+pref("extensions.jondofox.windows.buildID_override", "20140716183446");
 pref("extensions.jondofox.windows.oscpu_override", "Windows NT 6.1");
 pref("extensions.jondofox.windows.platform_override", "Win32");
 pref("extensions.jondofox.windows.productsub_override", "20100101");
-pref("extensions.jondofox.windows.useragent_override", "Mozilla/5.0 (Windows NT 6.1; rv:30.0) Gecko/20100101 Firefox/30.0");
+pref("extensions.jondofox.windows.useragent_override", "Mozilla/5.0 (Windows NT 6.1; rv:31.0) Gecko/20100101 Firefox/31.0");
 pref("extensions.jondofox.windows.useragent_vendor", "");
 pref("extensions.jondofox.windows.useragent_vendorSub", "");
 pref("extensions.jondofox.windows.accept_languages", "en-us,en");
@@ -114,7 +114,7 @@ pref("extensions.jondofox.windows.http.accept_encoding", "gzip, deflate");
 pref("extensions.jondofox.windows.default_charset", "ISO-8859-1");
 
 // Useragent settings FF Windows ESR
-pref("extensions.jondofox.windows_esr.buildID_override", "20140609182057");
+pref("extensions.jondofox.windows_esr.buildID_override", "20140714155506");
 pref("extensions.jondofox.windows_esr.useragent_override", "Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0");
 
 
